652ce51821f7b24db5ee4c49c60a52cf80f9cb61
fmabees <fmabees@live.com>
2022-04-28 18:10:07 -0700

    delete survey and edit

--- Shared/Response.cs
+++ Shared/Response.cs
@@ -22,6 +22,8 @@ namespace schedulesUnitedHosted.Shared
             this.Hour = Hour;
         }
 
+        public Response() {
+        }
         public string ToString()
         {
             return $"event: {EventId}, account: {AccId}, availability: {Availability}, hour: {Hour}";
